b9e656e97c656f774b5ad5c04fb96f6fb10865c7
Sefinek <contact@sefinek.net>
2024-12-04 15:23:46 +0100

    middlewares

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -2,11 +2,11 @@ const rateLimit = require('express-rate-limit');
 const RenderError = require('../services/renderError.js');
 
 module.exports = rateLimit({
-	windowMs: 30 * 1000,
-	limit: 50,
+	windowMs: 60 * 1000,
+	limit: 52,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
 
-	skip: req => req.ip === '::ffff:127.0.0.1' && process.env.NODE_ENV === 'development',
+	skip: () => process.env.NODE_ENV === 'development',
 	handler: (req, res) => RenderError(req, res, 429),
 });
--- middlewares/timeout.js
+++ middlewares/timeout.js
@@ -2,7 +2,7 @@ const timeout = require('express-timeout-handler');
 const RenderError = require('../services/renderError.js');
 
 module.exports = () => timeout.handler({
-	timeout: 4000,
+	timeout: 8000,
 	onTimeout: (req, res) => RenderError(req, res, 503),
 	disable: ['write', 'setHeaders', 'send', 'json', 'end'],
 });
